df08a961db793d82e381e1ea66ce3d107f74169e
Jessica Santos <annaeelza@gmail.com>
2023-11-27 15:56:37 -0300

    adicionar novo projeto

---
+++ projeto lampada/app.js
@@ -0,0 +1,31 @@
+'use strict'
+
+const ligar = document.getElementById('ligar')
+const desligar = document.getElementById('desligar')
+const lampada = document.getElementById('lampada')
+
+function lampadaQuebrada (){
+    return lampada.scroll.indexOf ('quebrada') > -1
+}
+
+function lampadaLigada (){
+    if(!lampadaQuebrada ()){
+        lampada.src = './img/ligada.jpg'
+    }
+}
+
+function lampadaDesligada (){
+    if(!lampadaQuebrada ()){
+        lampada.src = './img/desligada.jpg'
+    }
+}
+
+function lampQuebrada (){
+    lampada.src = './img/quebrada.jpg'
+}
+
+ligar.addEventListener( 'click', lampadaLigada)
+desligar.addEventListener( 'click', lampadaDesligada)
+lampada.addEventListener( 'mouseover', lampadaLigada)
+lampada.addEventListener( 'mouseover', lampadaDesligada)
+lampada.addEventListener( 'dblclick', lampQuebrada)
